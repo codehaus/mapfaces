b41970a0aa7d7b347213800d2257cb3f5c6211e0
mediii <mediii@435c697f-6949-0410-9f2e-81b55f2b53f5>
2008-09-10 12:07:41 +0000

    Creating a second module to build a war and a jar file, and fixed pom files. a setTransient was be added for the ContextComponent.

--- modules/library/main/src/main/java/org/mapfaces/renderkit/html/models/ContextRenderer.java
+++ modules/library/main/src/main/java/org/mapfaces/renderkit/html/models/ContextRenderer.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.mapfaces.renderkit.html.models;
 
 import java.io.FileReader;
@@ -31,157 +30,160 @@ import org.mapfaces.util.FacesUtils;
  * @author olivier
  */
 public class ContextRenderer extends Renderer {
-    
+
     private boolean debug;
-    
     private final String WIDGET_CSS = "/org/mapfaces/resources/css/widget.css";
     private final String OPENLAYERS_JS = "/org/mapfaces/resources/openlayers/custom/OpenLayers.js";
-    
+
     @Override
-    public void encodeBegin(FacesContext context, UIComponent component) throws IOException {        
+    public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
         try {
-            if(debug)
+            if (debug) {
                 System.out.println("ContextRenderer encodeBegin");
-            
-            
-            UIContext  comp = (UIContext) component;
+            }
+            UIContext comp = (UIContext) component;
             assertValid(context, component);
             ResponseWriter writer = context.getResponseWriter();
             /* writer.startElement("script", component);
-        writer.writeAttribute("type", "text/javascript", null);
-        writer.write("window.onload=\"alert('ici');\"");
-        writer.endElement("script");*/
-            
+            writer.writeAttribute("type", "text/javascript", null);
+            writer.write("window.onload=\"alert('ici');\"");
+            writer.endElement("script");*/
+
             //Add MapFaces css
             writer.startElement("link", component);
-            writer.writeAttribute("rel","stylesheet","rel");
+            writer.writeAttribute("rel", "stylesheet", "rel");
             writer.writeAttribute("href", ResourcePhaseListener.getURL(context, WIDGET_CSS, null), null);
             writer.writeAttribute("type", "text/css", null);
-            
-             //Add OpenLayers css
+
+            //Add OpenLayers css
             /*writer.endElement("link");
             writer.startElement("link", component);
             writer.writeAttribute("rel","stylesheet","rel");
             writer.writeAttribute("href", ResourcePhaseListener.getURL(context, "/org/mapfaces/resources/openlayers/theme/default/style.css", null), null);
             writer.writeAttribute("type", "text/css", null);
             writer.endElement("link");*/
-            
+
             //Add OpenLayers scripts
             writer.startElement("script", component);
             writer.writeAttribute("src", ResourcePhaseListener.getURL(context, OPENLAYERS_JS, null), null);
             writer.writeAttribute("type", "text/javascript", null);
             writer.endElement("script");
-            
+
             //Load context file
-            if(comp.getModel()==null){
+            if (comp.getModel() == null) {
                 String fileUrl = (String) component.getAttributes().get("service");
                 if (fileUrl == null || fileUrl.length() < 1) {
                     throw new IllegalArgumentException("You must indicate a path to file to read");
                 }
                 // Unmarshalles the given XML file to objects
-                JAXBContext Jcontext;      
-                Jcontext = JAXBContext.newInstance("net.opengis.owc.v030:net.opengis.context.v110");  
+                JAXBContext Jcontext;
+                Jcontext = JAXBContext.newInstance("net.opengis.owc.v030:net.opengis.context.v110");
                 Unmarshaller unmarshaller = Jcontext.createUnmarshaller();
                 ServletContext sc = (ServletContext) context.getExternalContext().getContext();
-                JAXBElement elt = (JAXBElement) unmarshaller.unmarshal(new FileReader(sc.getRealPath(fileUrl))); 
+                JAXBElement elt = (JAXBElement) unmarshaller.unmarshal(new FileReader(sc.getRealPath(fileUrl)));
                 comp.setJAXBElt(elt);
-           }else{
-                if(debug)
-                    System.out.println("AbstractContext already exist");   
-           }
-            
-           /* Add a4j:support component */
-           HtmlAjaxSupport ajaxComp = new HtmlAjaxSupport();
-           ajaxComp.setId(comp.getId()+"Ajax");
-           ajaxComp.setAjaxSingle(true);
-           ajaxComp.setImmediate(true);
-           comp.getChildren().add(ajaxComp);  
-           comp.setAjaxCompId(comp.getClientId(context)+"Ajax");
-                 
-        }   catch (JAXBException ex) {
+            } else {
+                if (debug) {
+                    System.out.println("AbstractContext already exist");
+                }
+            }
+
+            /* Add a4j:support component */
+            HtmlAjaxSupport ajaxComp = new HtmlAjaxSupport();
+            ajaxComp.setId(comp.getId() + "Ajax");
+            ajaxComp.setAjaxSingle(true);
+            ajaxComp.setImmediate(true);
+            comp.getChildren().add(ajaxComp);
+            comp.setAjaxCompId(comp.getClientId(context) + "Ajax");
+
+            component.setTransient(true);
+
+        } catch (JAXBException ex) {
             Logger.getLogger(ContextRenderer.class.getName()).log(Level.SEVERE, null, ex);
-        }   catch  (NullPointerException ex){
+        } catch (NullPointerException ex) {
             Logger.getLogger(ContextRenderer.class.getName()).log(Level.SEVERE, null, ex);
         }
-           
-      
-        
+
+
+
     }
-    
+
     @Override
     public void encodeChildren(FacesContext context, UIComponent component) throws IOException {
-        if(debug)
+        if (debug) {
             System.out.println("ContextRenderer  children");
-        List<UIComponent> childrens  = component.getChildren();        
-        for(UIComponent tmp : childrens){
-            FacesUtils.encodeRecursive(context,tmp);
         }
-    }     
+        List<UIComponent> childrens = component.getChildren();
+        for (UIComponent tmp : childrens) {
+            FacesUtils.encodeRecursive(context, tmp);
+        }
+    }
 
     @Override
     public boolean getRendersChildren() {
-        
+
         return true;
     }
-       
+
     @Override
-     public void encodeEnd(FacesContext context, UIComponent component) throws IOException { 
-        UIContext comp = (UIContext) component;   
-        if(debug)
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        UIContext comp = (UIContext) component;
+        if (debug) {
             System.out.println("ContextRenderer encodeEnd");
+        }
         ResponseWriter writer = context.getResponseWriter();
-       
+
         writer.flush();
     }
-    
-    
+
     @Override
-    public void decode(FacesContext context, UIComponent component) {  
-        UIContext comp = (UIContext) component;          
-        if(debug){
+    public void decode(FacesContext context, UIComponent component) {
+        UIContext comp = (UIContext) component;
+        if (debug) {
             System.out.println("ContextRenderer decode");
         }
-        if(context.getExternalContext().getRequestParameterMap()!=null){
-            AbstractContext tmp =comp.getModel();
+        if (context.getExternalContext().getRequestParameterMap() != null) {
+            AbstractContext tmp = comp.getModel();
             //tmp.setI
             Map params = context.getExternalContext().getRequestParameterMap();
-           /* String bbox = (String) params.get(comp.getParent().getId()+":bbox");               
+            /* String bbox = (String) params.get(comp.getParent().getId()+":bbox");               
             if(bbox!=null && !bbox.equals(tmp.getMinx()+","+tmp.getMiny().toString()+","+tmp.getMaxx()+","+tmp.getMaxy())){ 
-                try {
-                    System.out.println("voici la nouveau bboxxxxxxxxx : "+bbox);
-                    if( !tmp.getSrs().equals("urn:ogc:def:crs:EPSG:6.6:4326") )
-                        bbox = (String) GeometricUtilities.reprojectBbox2DString("urn:ogc:def:crs:EPSG:6.6:4326",tmp.getSrs(),bbox);
-                    System.out.println("voici la nouveau bboxxxxxxxxx : "+bbox);
-                    tmp.setMinx(new Double(bbox.split(",")[0]));
-                    tmp.setMiny(new Double(bbox.split(",")[1]));
-                    tmp.setMaxx(new Double(bbox.split(",")[2]));
-                    tmp.setMaxy(new Double(bbox.split(",")[3]));
-                } catch (NoSuchAuthorityCodeException ex) {
-                    Logger.getLogger(ContextRenderer.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (FactoryException ex) {
-                    Logger.getLogger(ContextRenderer.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (TransformException ex) {
-                    Logger.getLogger(ContextRenderer.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }*/
-            
-            String title = (String) params.get(FacesUtils.getFormId(context, component)+":title"); 
-            if(title!=null  &&  !title.equals(tmp.getTitle())){
-                tmp.setTitle(title);   
+            try {
+            System.out.println("voici la nouveau bboxxxxxxxxx : "+bbox);
+            if( !tmp.getSrs().equals("urn:ogc:def:crs:EPSG:6.6:4326") )
+            bbox = (String) GeometricUtilities.reprojectBbox2DString("urn:ogc:def:crs:EPSG:6.6:4326",tmp.getSrs(),bbox);
+            System.out.println("voici la nouveau bboxxxxxxxxx : "+bbox);
+            tmp.setMinx(new Double(bbox.split(",")[0]));
+            tmp.setMiny(new Double(bbox.split(",")[1]));
+            tmp.setMaxx(new Double(bbox.split(",")[2]));
+            tmp.setMaxy(new Double(bbox.split(",")[3]));
+            } catch (NoSuchAuthorityCodeException ex) {
+            Logger.getLogger(ContextRenderer.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (FactoryException ex) {
+            Logger.getLogger(ContextRenderer.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (TransformException ex) {
+            Logger.getLogger(ContextRenderer.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
+            }*/
+
+            String title = (String) params.get(FacesUtils.getFormId(context, component) + ":title");
+            if (title != null && !title.equals(tmp.getTitle())) {
+                tmp.setTitle(title);
+            }
+
             /*String win = (String) params.get(comp.getParent().getId()+":window");  
             if( win != null){
-                String[] window = win.split(",");
-                tmp.setWindowWidth(new BigInteger(window[0]));
-                tmp.setWindowHeight(new BigInteger(window[1]));
+            String[] window = win.split(",");
+            tmp.setWindowWidth(new BigInteger(window[0]));
+            tmp.setWindowHeight(new BigInteger(window[1]));
             }*/
             comp.setModel(tmp);
-            if(debug)
-                System.out.println("    Nouveaux parametres du context : "+tmp.getTitle()+" "+tmp.getMinx()+" "+tmp.getMiny().toString()+" "+tmp.getMaxx()+" "+tmp.getMaxy()+"");
+            if (debug) {
+                System.out.println("    Nouveaux parametres du context : " + tmp.getTitle() + " " + tmp.getMinx() + " " + tmp.getMiny().toString() + " " + tmp.getMaxx() + " " + tmp.getMaxy() + "");
+            }
         }
     }
-    
+
     private void assertValid(FacesContext context, UIComponent component) {
         if (context == null) {
             throw new NullPointerException("context should not be null");
@@ -197,5 +199,4 @@ public class ContextRenderer extends Renderer {
     public void setDebug(boolean debug) {
         this.debug = debug;
     }
-
 }
