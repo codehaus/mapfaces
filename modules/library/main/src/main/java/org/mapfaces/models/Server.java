15c9d706bc10e96658f316303c2ee5ae6516b4e2
oterral <oterral@435c697f-6949-0410-9f2e-81b55f2b53f5>
2008-09-25 14:22:38 +0000

    add Context and Server interface

---
+++ modules/library/main/src/main/java/org/mapfaces/models/Server.java
@@ -0,0 +1,36 @@
+/*
+ *    Mapfaces - 
+ *    http://www.mapfaces.org
+ *
+ *    (C) 2007 - 2008, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General 
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General  License for more details.
+ */
+
+package org.mapfaces.models;
+
+import java.io.Serializable;
+
+public interface  Server  extends Serializable{
+    
+    String getHref();
+    
+    void setHref(String href);
+        
+    String getService();
+    
+    void setService(String service);
+    
+    String getVersion();
+    
+    void setVersion(String version);
+
+}
