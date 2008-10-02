7b4ec09d19eaba30fd3c52535b35753539740e9d
mediii <mediii@435c697f-6949-0410-9f2e-81b55f2b53f5>
2008-10-02 08:38:37 +0000

    Adding a temporary serialization for the DefaultServer class, especially for the WMSCapabilities object.

--- modules/library/main/src/main/java/org/mapfaces/models/Serial.java
+++ modules/library/main/src/main/java/org/mapfaces/models/Serial.java
@@ -1,11 +1,23 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *    Mapfaces - 
+ *    http://www.mapfaces.org
+ *
+ *    (C) 2007 - 2008, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General 
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General  License for more details.
  */
+
 package org.mapfaces.models;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -19,18 +31,28 @@ import java.io.Serializable;
  */
 public class Serial implements Serializable {
 
+    /**
+    +     * Determines if a de-serialized file is compatible with this class.
+    +     *
+    +     * Maintainers must change this value if and only if the new version
+    +     * of this class is not compatible with old versions. See Sun docs
+    +     * for <a href=http://java.sun.com/products/jdk/1.1/docs/guide
+    +     * /serialization/spec/version.doc.html> details. </a>
+    +     *
+    +     * Not necessary to include in first version of the class, but
+    +     * included here as a reminder of its importance.
+    +     */
     private static final long serialVersionUID = 7526471155622776147L;
-
     private String name;
-    
     private transient Dimension dimension;
-    
     static private Serial singleton = null;
 
+    /**
+     * Empty constructor used for Serialization.
+     */
     public Serial() {
-        
     }
-    
+
     public Serial(String name, Dimension d) {
         this.name = name;
         this.dimension = d;
@@ -54,15 +76,14 @@ public class Serial implements Serializable {
 
     /*@Override
     public String toString() {
-        return "[ " + this.getClass().getSimpleName() + " : " + this.name + " : " + this.dimension + " ] ";
+    return "[ " + this.getClass().getSimpleName() + " : " + this.name + " : " + this.dimension + " ] ";
     }*/
-
     public static void main(String... args) {
 
         try {
             Dimension d = new DefaultDimension();
             d.setCurrent(true);
-            
+
             Serial serial = new Serial("monserial", d);
             System.out.println(serial + " : " + serial.getDimension().isCurrent());
 
@@ -95,21 +116,21 @@ public class Serial implements Serializable {
             ioe.printStackTrace();
         }
     }
-    
+
     Object writeReplace() throws ObjectStreamException {
-        System.out.println("Entering writeReplace");
-        
-        singleton = this;
-        Serial s = getSingleton();
-                
+        Dimension dim = getDimension();
+        Serial tmp = getSingleton();
+        tmp.setDimension(dim);
+        Serial s = this;
+
         return s;
     }
 
     Object readResolve() throws ObjectStreamException {
-        System.out.println("Entering readResolve");
-        
-        Serial s = getSingleton();
-        
+        Serial s = this;
+        Dimension dim = getSingleton().getDimension();
+        s.setDimension(dim);
+
         return s;
     }
 
