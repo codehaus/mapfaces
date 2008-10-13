5aa6cbc0383567477799061bf97f56c072c7e7c7
mediii <mediii@435c697f-6949-0410-9f2e-81b55f2b53f5>
2008-10-13 08:39:47 +0000

    Added a reportray process while an exception has occured in the portray method, and fixed some getters in the TreeItem object.

--- trunk/modules/library/main/src/main/java/org/mapfaces/renderkit/html/LayerRenderer.java
+++ trunk/modules/library/main/src/main/java/org/mapfaces/renderkit/html/LayerRenderer.java
@@ -118,12 +118,22 @@ public class LayerRenderer extends WidgetBaseRenderer {
                         model.setMaxy(String.valueOf(env.getMaxY()));
                     }
                     System.out.println("=============== portraying " + env);
-                    
+
                     try {
-                    FacesUtils.getParentUIMapPane(context, component).getPortray().portray(defaultMapContext, env, dst, layer.getOutputFormat(), new Dimension(new Integer(width), new Integer(height)), false);
-                    }
-                    catch (org.geotools.factory.RecursiveSearchException e){
-                        System.out.println("Catched Exception : "+e.getMessage());
+                        FacesUtils.getParentUIMapPane(context, component).getPortray().portray(defaultMapContext, env, dst, layer.getOutputFormat(), new Dimension(new Integer(width), new Integer(height)), false);
+                    } catch (org.geotools.factory.RecursiveSearchException e) {
+                        System.out.println("Catched Exception : " + e.getMessage());
+                        org.geotools.factory.RecursiveSearchException exp = e;
+                        while (exp != null) {
+                            try {
+                                System.out.println("======== doing a reportay process while the exception is occured ! ");
+                                FacesUtils.getParentUIMapPane(context, component).getPortray().portray(defaultMapContext, env, dst, layer.getOutputFormat(), new Dimension(new Integer(width), new Integer(height)), false);
+                                exp = null;
+                            } catch (org.geotools.factory.RecursiveSearchException exception) {
+                                exp = exception;
+                                System.out.println(">>>>> "+exp.getMessage());
+                            }
+                        }
                     }
                     System.out.println("            Layer generate file finish " + layer.getName());
                     writer.startElement("img", comp);
