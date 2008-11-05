8f0cfc7bd2aa79352e1ada525215e4c17de78704
eclesia <eclesia@435c697f-6949-0410-9f2e-81b55f2b53f5>
2008-11-05 15:49:11 +0000

    Clean up mapfaces treelayout package

--- trunk/modules/library/main/src/main/java/org/mapfaces/component/treelayout/UISelectOneMenuColumn.java
+++ trunk/modules/library/main/src/main/java/org/mapfaces/component/treelayout/UISelectOneMenuColumn.java
@@ -1,36 +1,65 @@
+/*
+ *    Mapfaces -
+ *    http://www.mapfaces.org
+ *
+ *    (C) 2007 - 2008, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package org.mapfaces.component.treelayout;
 
 import org.mapfaces.component.abstractTree.UIColumnBase;
 
+/**
+ * @author Kevin Delfour (Geomatys)
+ */
 public class UISelectOneMenuColumn extends UIColumnBase{
 
-    private final String RENDERER_TYPE = "org.mapfaces.renderkit.treelayout.HTMLSelectOneMenuColumn";
-    private final String FAMILY = "org.mapfaces.treelayout.Column";
-    
-     /**
-     * Add extra parameter like this
-     * 
-     */
+    private static final String RENDERER_TYPE = "org.mapfaces.renderkit.treelayout.HTMLSelectOneMenuColumn";
+    private static final String FAMILY = "org.mapfaces.treelayout.Column";
+
     private String title = null;
     private String itemsLabels = "300*150/300*300/600*300";
-    private String itemsValues = "300,150/300,300/600,300";    
-     private String separator="/";
-//
-     @Override
+    private String itemsValues = "300,150/300,300/600,300";
+    private String separator="/";
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
     public String getFamily() {
         return FAMILY;
     }
 
+    /**
+     * {@inheritDoc }
+     */
     @Override
     public String getRendererType() {
         return RENDERER_TYPE;
     }
 
+    /**
+     * {@inheritDoc }
+     */
+    @Override
     public String getHeaderTitle() {
         return title;
     }
 
-    public void setHeaderTitle(String title) {
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public void setHeaderTitle(final String title) {
         this.title = title;
     }
 
@@ -38,7 +67,7 @@ public class UISelectOneMenuColumn extends UIColumnBase{
         return itemsLabels;
     }
 
-    public void setItemsLabels(String itemsLabels) {
+    public void setItemsLabels(final String itemsLabels) {
         this.itemsLabels = itemsLabels;
     }
 
@@ -46,7 +75,7 @@ public class UISelectOneMenuColumn extends UIColumnBase{
         return itemsValues;
     }
 
-    public void setItemsValues(String itemsValues) {
+    public void setItemsValues(final String itemsValues) {
         this.itemsValues = itemsValues;
     }
 
@@ -54,7 +83,7 @@ public class UISelectOneMenuColumn extends UIColumnBase{
         return separator;
     }
 
-    public void setSeparator(String separator) {
+    public void setSeparator(final String separator) {
         this.separator = separator;
     }
 }
