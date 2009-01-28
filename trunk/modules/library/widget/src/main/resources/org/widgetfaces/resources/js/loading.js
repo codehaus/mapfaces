213a5ad5181b8ad742d1c0d40b5e185d9f844ed1
kdelfour <kdelfour@435c697f-6949-0410-9f2e-81b55f2b53f5>
2009-01-28 08:10:26 +0000

    Added compressed js for datepicker and autocompletion, modify loading.js for load mootools library in one js only

--- trunk/modules/library/widget/src/main/resources/org/widgetfaces/resources/js/loading.js
+++ trunk/modules/library/widget/src/main/resources/org/widgetfaces/resources/js/loading.js
@@ -7,31 +7,7 @@ Copyright:
 
 var headID = document.getElementsByTagName("head")[0];
 var browser = navigator.appName;
-var scr_mootools,scr_mootools_more, link;
-/*
- * Test if Mootools is present on the page
- */
-//try{
-//    if (MooTools != null){
-////console.log('Mootools already loaded');
-//}
-//}catch(err){
-//    scr_mootools = document.createElement('script');
-//    scr_mootools.setAttribute('type', 'text/javascript');
-//    scr_mootools.setAttribute('src', 'resource.jsf?r=/org/widgetfaces/resources/js/mootools-1.2-core.js');
-//    document.getElementsByTagName('head')[0].appendChild(scr_mootools);
-//}
-//
-//try{
-//    if (MooToolsMore != null){
-////console.log('Mootools already loaded');
-//}
-//}catch(err){
-//    scr_mootools_more = document.createElement('script');
-//    scr_mootools_more.setAttribute('type', 'text/javascript');
-//    scr_mootools_more.setAttribute('src', 'resource.jsf?r=/org/widgetfaces/resources/js/mootools-1.2-more.js');
-//    document.getElementsByTagName('head')[0].appendChild(scr_mootools_more);
-//}
+var scr_mootools;
 
 try{
     if (MooTools != null){
