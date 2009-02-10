a64145eaefdb5c5e0ff8aee9141d494250ad4c50
oterral <oterral@435c697f-6949-0410-9f2e-81b55f2b53f5>
2009-02-10 13:33:40 +0000

    Fix bug when empty dataset, now a chart is drawed

--- trunk/modules/library/chart/src/main/resources/org/mapfaces/resources/js/init.js
+++ trunk/modules/library/chart/src/main/resources/org/mapfaces/resources/js/init.js
@@ -3,8 +3,9 @@ function loadOpenCharts(id) {
     var canvas = new OpenLayers.Map(id, {canvasContainer : document.getElementById(id+'Container')});
 }
 
-if (document.getElementById('canvas') && window.addEvents) {
-    addEvents('canvas');
+if (document.getElementById('canvas')) {
+    if (window.addEvents)
+        addEvents('canvas');
     if (window.OpenLayers && document.getElementById('canvasContainer') && window.loadOpenCharts)
         loadOpenCharts('canvas')
 }
