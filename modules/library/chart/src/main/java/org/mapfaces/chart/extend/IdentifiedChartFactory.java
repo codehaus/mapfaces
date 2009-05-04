04460981dba1ede5b1af7e5f84df55fe5d2a0b06
eclesia <eclesia@435c697f-6949-0410-9f2e-81b55f2b53f5>
2009-05-04 16:00:10 +0000

    Fix some missing headers

--- modules/library/chart/src/main/java/org/mapfaces/chart/extend/IdentifiedChartFactory.java
+++ modules/library/chart/src/main/java/org/mapfaces/chart/extend/IdentifiedChartFactory.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *    Mapfaces - http://www.mapfaces.org
+ *
+ *    (C) 2009, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 
 package org.mapfaces.chart.extend;
