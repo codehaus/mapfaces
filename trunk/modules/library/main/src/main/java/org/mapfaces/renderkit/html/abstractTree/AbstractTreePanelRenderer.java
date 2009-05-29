9a744aa73f355c913f0e29899ac33c0659babbc5
mediii <mediii@435c697f-6949-0410-9f2e-81b55f2b53f5>
2009-05-29 14:30:52 +0000

    Rename by refactor class from servletUtils to ServletUtils.

--- trunk/modules/library/main/src/main/java/org/mapfaces/renderkit/html/abstractTree/AbstractTreePanelRenderer.java
+++ trunk/modules/library/main/src/main/java/org/mapfaces/renderkit/html/abstractTree/AbstractTreePanelRenderer.java
@@ -42,7 +42,7 @@ import org.mapfaces.models.tree.TreeTableModel;
 import org.mapfaces.share.interfaces.AjaxRendererInterface;
 import org.mapfaces.share.interfaces.CustomizeTreeComponentRenderer;
 import org.mapfaces.share.listener.ResourcePhaseListener;
-import org.mapfaces.share.request.servletUtils;
+import org.mapfaces.share.request.ServletUtils;
 import org.mapfaces.share.utils.Utils;
 import org.mapfaces.util.AjaxUtils;
 import org.mapfaces.util.tree.TreeStyle;
@@ -85,7 +85,7 @@ public abstract class AbstractTreePanelRenderer extends Renderer implements Ajax
         /*
          * Look client Browser, if it's Opera, we fix loadAll option
          */
-        if (servletUtils.isOpera()) {
+        if (ServletUtils.isOpera()) {
             loadAll = true;
         } else {
             loadAll = treepanel.isLoadAll();
