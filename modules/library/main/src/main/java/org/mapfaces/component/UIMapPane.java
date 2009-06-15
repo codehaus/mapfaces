c14cb1674ef323b3bec6c661d93654642730475e
oterral <oterral@435c697f-6949-0410-9f2e-81b55f2b53f5>
2009-06-15 07:32:54 +0000

    Add getScale() function

--- modules/library/main/src/main/java/org/mapfaces/component/UIMapPane.java
+++ modules/library/main/src/main/java/org/mapfaces/component/UIMapPane.java
@@ -16,11 +16,22 @@
  */
 package org.mapfaces.component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 
+import javax.measure.unit.SI;
 import org.geotoolkit.map.MapContext;
+import org.geotoolkit.measure.Measure;
+import org.geotoolkit.referencing.CRS;
+import org.geotoolkit.referencing.crs.AbstractCRS;
+import org.mapfaces.models.Context;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
 
 /**
  * @author Olivier Terral.
@@ -29,6 +40,32 @@ import org.geotoolkit.map.MapContext;
 public class UIMapPane extends UIWidgetBase {
 
     public static final String FAMILIY = "org.mapfaces.MapPane";
+    //public static final double INCHES_PER_UNIT = 4374754;
+
+    static final HashMap<String, Double> INCHES_PER_UNIT = new HashMap<String, Double>();
+    static {
+        INCHES_PER_UNIT.put("inches", 1.0);
+        INCHES_PER_UNIT.put("ft", 12.0);
+        INCHES_PER_UNIT.put("mi", 63360.0);
+        INCHES_PER_UNIT.put("m", 39.3701);
+        INCHES_PER_UNIT.put("km", 39370.1);
+        INCHES_PER_UNIT.put("dd", 4374754.0);
+        INCHES_PER_UNIT.put("yd", 36.0);
+    }
+//    {
+//        'inches': 1.0,
+//    'ft': 12.0,
+//    'mi': 63360.0,
+//    'm': 39.3701,
+//    'km': 39370.1,
+//    'dd': 4374754,
+//    'yd': 36
+//};
+    /**
+     * Constant: DOTS_PER_INCH
+     * {Integer} 72 (A sensible default)
+     */
+    public static final int DOTS_PER_INCH = 72;
     private MapContext defaultMapContext;
     /**
      * for untiled wms layers: how many times should the map image be
@@ -74,7 +111,7 @@ public class UIMapPane extends UIWidgetBase {
     private List<Float> resolutions = null;
     //fixed scales - overrides resolutions
     private List<Float> scales = null;
-    private String units = "degrees";
+    private String units = "dd";
     private Boolean fixedSize = false;
     private Boolean fractionalZoom = true;
     private Boolean singleTile = true;
@@ -97,6 +134,33 @@ public class UIMapPane extends UIWidgetBase {
 
     public String getAjaxCompId() {
         return ajaxCompId;
+    }
+
+    public Double getScale(Context model) {
+//        try {
+        final int width = Integer.valueOf(model.getWindowWidth());
+        final double widthBbox = Double.valueOf(model.getMaxx()) - Double.valueOf(model.getMinx());
+//        System.out.println(CRS.decode(model.getSrs()).getCoordinateSystem().getAxis(0).getUnit().getDimension().toString());
+        return ((widthBbox / width) * INCHES_PER_UNIT.get(units) * DOTS_PER_INCH);
+//            final double minx = Double.valueOf(model.getMinx());
+//            final double maxx = Double.valueOf(model.getMaxx());
+//            final double miny = Double.valueOf(model.getMiny());
+//            final double maxy = Double.valueOf(model.getMaxy());
+////            final double minx = Double.valueOf("-179");
+////            final double maxx = Double.valueOf("179");
+////            final double miny = Double.valueOf("-90");
+////            final double maxy = Double.valueOf("90");
+//            final double centery = (maxy+miny)/2;
+//            //@TODO specific to GeoTK
+//            Measure mes = ((AbstractCRS) CRS.decode("EPSG:4326")).distance(new double[]{minx,centery}, new double[]{maxx,centery});
+//            System.out.println(mes.doubleValue());
+//            return mes.getUnit().getConverterTo(SI.KILO(SI.METRE)).convert(mes.doubleValue());
+//        } catch (NoSuchAuthorityCodeException ex) {
+//            Logger.getLogger(UIMapPane.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (FactoryException ex) {
+//            Logger.getLogger(UIMapPane.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return null;
     }
 
     public void setAjaxCompId(final String ajaxCompId) {
