944453b4c1090cfd9befaf53520585a4de53d7a6
oterral <oterral@435c697f-6949-0410-9f2e-81b55f2b53f5>
2009-08-03 11:00:15 +0000

    Remove Metadata property

--- trunk/modules/library/main/src/main/java/org/mapfaces/models/layer/DefaultLayer.java
+++ trunk/modules/library/main/src/main/java/org/mapfaces/models/layer/DefaultLayer.java
@@ -26,7 +26,6 @@ import org.mapfaces.models.DescriptionURL;
 import org.mapfaces.models.Dimension;
 import org.mapfaces.models.Layer;
 import org.mapfaces.models.LayerType;
-import org.mapfaces.models.metadata.Metadata;
 import org.opengis.geometry.Envelope;
 
 public class DefaultLayer implements Layer {
@@ -64,7 +63,6 @@ public class DefaultLayer implements Layer {
     private String inlineGeometry;
     private String dataUrl;
     private String metadataUrl;
-    private Metadata metadata;
     private String responseCRS;
     private String depth;
     private String resX;
@@ -776,14 +774,5 @@ public class DefaultLayer implements Layer {
     public boolean isDisable() {
         return disable;
     }
-
-    public Metadata getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(Metadata metadata) {
-        this.metadata = metadata;
-    }
-
 
 }
