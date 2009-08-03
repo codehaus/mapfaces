5cf2713c90663b5fd7e6db4c73ff71ce4772f78b
oterral <oterral@435c697f-6949-0410-9f2e-81b55f2b53f5>
2009-08-03 10:24:55 +0000

    Cleaning code

--- trunk/modules/template/src/main/java/org/mapfaces/template/bean/InitBean.java
+++ trunk/modules/template/src/main/java/org/mapfaces/template/bean/InitBean.java
@@ -3,7 +3,6 @@
 package org.mapfaces.template.bean;
 
 import java.util.Locale;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.context.FacesContext;
 
@@ -16,18 +15,14 @@ public class InitBean {
     /**
      * Allow to hide some message used by devellopers and to log information in data file.
      */
-    private static final Logger LOGGER = Logger.getLogger(InitBean.class.getName());
+    //private static final Logger LOGGER = Logger.getLogger(InitBean.class.getName());
     
     private Locale locale;
     
     
 
     public InitBean() {
-        initJSF();
-    }
-
-    public void initJSF() {
-        FacesContext context = FacesContext.getCurrentInstance();
+        final FacesContext context = FacesContext.getCurrentInstance();
         if (context.getViewRoot().getLocale() == null) {
             context.getViewRoot().setLocale(Locale.ENGLISH);
             locale = Locale.ENGLISH;
