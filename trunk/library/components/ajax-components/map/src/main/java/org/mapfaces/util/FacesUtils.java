f7795250cb12b81f0f7be78c3b0316d35e277693
mediii <mediii@435c697f-6949-0410-9f2e-81b55f2b53f5>
2009-08-17 15:11:03 +0000

    Map module : applying transform and factorization of taglib and cleanup.

--- trunk/library/components/ajax-components/map/src/main/java/org/mapfaces/util/FacesUtils.java
+++ trunk/library/components/ajax-components/map/src/main/java/org/mapfaces/util/FacesUtils.java
@@ -1193,7 +1193,6 @@ public class FacesUtils extends org.mapfaces.share.utils.FacesUtils{
      * @return PhaseListener
      */
     public static PhaseListener getListenerFromLifeCycle(Class<?> c) {
-        //getting the DetectBrowserListener if is exists, else uses the FacesUtils method.
         final LifecycleFactory factory = (LifecycleFactory) FactoryFinder.getFactory(FactoryFinder.LIFECYCLE_FACTORY);
         final Lifecycle lifecycle = factory.getLifecycle(LifecycleFactory.DEFAULT_LIFECYCLE);
         final PhaseListener[] listeners = lifecycle.getPhaseListeners();
