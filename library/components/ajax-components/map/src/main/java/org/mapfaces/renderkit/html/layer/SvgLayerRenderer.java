ce1f41ffa8dd95f19f39f25b30b463ff3b166492
lpratlong <lpratlong@435c697f-6949-0410-9f2e-81b55f2b53f5>
2009-11-04 17:02:59 +0000

    SVGLayer component : data transfert from server component to client component -> modification on the renderer / bug fixes.

--- library/components/ajax-components/map/src/main/java/org/mapfaces/renderkit/html/layer/SvgLayerRenderer.java
+++ library/components/ajax-components/map/src/main/java/org/mapfaces/renderkit/html/layer/SvgLayerRenderer.java
@@ -20,9 +20,12 @@ package org.mapfaces.renderkit.html.layer;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
+import com.vividsolutions.jts.io.WKTWriter;
 import org.mapfaces.renderkit.html.*;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -34,27 +37,20 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import org.geotoolkit.data.collection.FeatureCollection;
 import org.geotoolkit.data.collection.FeatureIterator;
-import org.geotoolkit.feature.simple.SimpleFeatureBuilder;
-import org.geotoolkit.feature.simple.SimpleFeatureTypeBuilder;
-import org.geotoolkit.feature.type.DefaultGeometryType;
-import org.geotoolkit.metadata.iso.extent.DefaultGeographicDescription;
 import org.geotoolkit.referencing.CRS;
-import org.geotoolkit.referencing.crs.DefaultGeographicCRS;
 import org.mapfaces.component.UIMapPane;
 import org.mapfaces.component.layer.UISvgLayer;
 import org.mapfaces.component.models.UIContext;
 import org.mapfaces.models.BasicFeature;
 import org.mapfaces.models.Context;
+import org.mapfaces.models.DefaultFeature;
 import org.mapfaces.models.Feature;
 import org.mapfaces.share.utils.RendererUtils.HTML;
 import org.mapfaces.util.FacesMapUtils;
-import org.opengis.feature.FeatureFactory;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
-import org.opengis.metadata.extent.GeographicDescription;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
-import org.opengis.referencing.crs.GeographicCRS;
 
 /**
  *
@@ -69,10 +65,7 @@ public class SvgLayerRenderer extends WidgetBaseRenderer {
     @Override
     public void encodeBegin(final FacesContext context, final UIComponent component) throws IOException {
 
-        System.out.println("===========> ENTREE DANS LE ENCODE");
         super.encodeBegin(context, component);
-
-        System.out.println("===========> APRES SUPER");
         //Find UIMapPane refers to this widget
         String mapJsVariable = null ;
         final UIMapPane uIMapPane = FacesMapUtils.getUIMapPane(context, component);
@@ -83,9 +76,7 @@ public class SvgLayerRenderer extends WidgetBaseRenderer {
             component.setRendered(false);
             return;
         }
-        
-        System.out.println("===========> Map JS = " + mapJsVariable);
-        System.out.println("===========> CONTINUE");
+
         final UISvgLayer comp = (UISvgLayer) component;
         final String clientId = comp.getClientId(context);
         final String compId;
@@ -103,19 +94,17 @@ public class SvgLayerRenderer extends WidgetBaseRenderer {
         writer.writeAttribute(HTML.TYPE_ATTR, HTML.INPUT_TYPE_HIDDEN, HTML.TYPE_ATTR);
         writer.writeAttribute(HTML.NAME_ATTRIBUTE, clientId, HTML.NAME_ATTRIBUTE);
         writer.writeAttribute(HTML.id_ATTRIBUTE, clientId, HTML.id_ATTRIBUTE);
-        writer.writeAttribute(HTML.value_ATTRIBUTE, comp.getValue(), HTML.value_ATTRIBUTE);
         writer.endElement((String) HTML.INPUT_ELEM);
-        
-        System.out.println("========>  COMPID = " + compId);
         
         writer.startElement(HTML.SCRIPT_ELEM, comp);
         writer.writeAttribute(HTML.TYPE_ATTR, "text/javascript", "text/javascript");
         // SVG Layer for OpenLayers is created.
         final String functionName = "addFeature_" + compId;
+        final String layerName = "window." + compId;
         writer.write(new StringBuilder("").append("" +
-                "window." + compId + " = new OpenLayers.Layer.Vector('" + compId + "');" +
-                "window." + compId + ".events.register('featureadded', " + compId + "" + ", " + functionName + ");" +
-                mapJsVariable + ".addLayers(window." + compId + ");" +
+                layerName + " = new OpenLayers.Layer.Vector('" + compId + "');" +
+                layerName + ".events.register('featureadded', " + compId + "" + ", " + functionName + ");" +
+                mapJsVariable + ".addLayers(" + layerName + ");" +
                 "if($('"+ clientId + "')){$('" + clientId + "').value = 'a;' + " + mapJsVariable + ".getProjection();}" ).toString());
 
         // When a feature is created, we keep the coordinates in a hidden field.
@@ -123,6 +112,22 @@ public class SvgLayerRenderer extends WidgetBaseRenderer {
                 "function " + functionName + "(event){" + "if($('"+ clientId + "')){" +
                         "$('" + clientId + "').value=$('" + clientId + "').value" +
                         "+ ';' + event.feature.geometry;}}").toString());
+
+        if ((comp.getValue() != null) && (comp.getValue() instanceof List)) {
+            final List<SimpleFeature> featList = (List) comp.getValue();
+            if (featList.size() > 0) {
+                writer.write("var parser_" + compId + ",var wkt_" + compId + ",var geometry_" + compId + ",var feature_" + compId + ";");
+                final WKTWriter wktWriter = new WKTWriter();
+                for (final SimpleFeature feature : featList) {
+                    writer.write(new StringBuilder("").append("parser_" + compId + " = new OpenLayers.Format.WKT();" +
+                            "wkt_" + compId + " = '" + wktWriter.write((Geometry) feature.getDefaultGeometry()) + "';" +
+                            "geometry_" + compId + " = parser.read(wkt_" + compId + ");" +
+                            "feature_" + compId + " = new OpenLayers.Feature.Vector(geometry_" + compId + ");" +
+                            layerName + ".addFeatures(feature_" + compId + ");").toString());
+                }
+            }
+        }
+        
         writer.endElement(HTML.SCRIPT_ELEM);
         writer.flush();
     }
@@ -139,7 +144,6 @@ public class SvgLayerRenderer extends WidgetBaseRenderer {
             final UISvgLayer comp = (UISvgLayer) component;
             final Map parameterMap = ext.getRequestParameterMap();
             
-            System.out.println("DECODE");
             if (this.debug) {
                 LOGGER.log(Level.INFO, "[DEBUG] ScgLayerRenderer DECODE");
             }
@@ -148,54 +152,41 @@ public class SvgLayerRenderer extends WidgetBaseRenderer {
 
             final Map<String, String> params = context.getExternalContext().getRequestParameterMap();
             if (params != null) {
-                System.out.println("==> PREMIER IF");
-                for (Entry e : params.entrySet()) {
-                    System.out.println("--------" + e.getKey() + " ; " + e.getValue());
-                }
                 final String value = (String) parameterMap.get(comp.getClientId(context));
 
                 if ((value != null) && !value.isEmpty()) {
-                    System.out.println("==> SECOND IF");
-                    System.out.println("==> VALEUR = " + value);
-                    final String[] featList = value.split(";");
-                    final List<Feature> features = new ArrayList<Feature>();
-                    if (featList.length > 2) {
-                        System.out.println("==> TROISIEME IF");
-                        final String operation = featList[0];
-                        WKTReader wktReader = new WKTReader();
-                        final CoordinateReferenceSystem crs = CRS.decode(featList[1]);
-                        for (int i = 2; i < featList.length; i++) {
-                            System.out.println("++++++ A");
-                            Feature feat = new BasicFeature();
-                            System.out.println("++++++ B");
-                            feat.setGeometry(wktReader.read(featList[i]));
-                            System.out.println("++++++ C");
-                            feat.setCrs(crs);
-                            System.out.println("++++++ D");
-                            feat.setName("feature" + i);
-                            System.out.println("++++++ E");
-                            features.add(feat);
-                        }
+                    if (value instanceof String) {
+                        final String[] featList = value.split(";");
+                        final List<Feature> features = new ArrayList<Feature>();
+                        if (featList.length > 2) {
+                            final String operation = featList[0];
+                            final WKTReader wktReader = new WKTReader();
+                            final CoordinateReferenceSystem crs = CRS.decode(featList[1]);
+                            for (int i = 2; i < featList.length; i++) {
+                                Feature feat = new DefaultFeature();
+                                feat.setAttributes(new HashMap<String, Serializable>());
+                                feat.setGeometry(wktReader.read(featList[i]));                                
+                                feat.getAttributes().put("geometry", wktReader.read(featList[i]));
+                                feat.setCrs(crs);
+                                feat.setName("feature" + i);
+                                features.add(feat);
+                            }
 
-                        FeatureCollection<SimpleFeatureType, SimpleFeature> simpleFeatures = FacesMapUtils.getSimpleFeaturesFromFeatures(features);
-                        System.out.println("++++++ F");
-                        List<SimpleFeature> sfList = new ArrayList<SimpleFeature>();
-                        FeatureIterator<SimpleFeature> featIt = simpleFeatures.features();
-                        while(featIt.hasNext()) {
-                            sfList.add(featIt.next());
-                        }
-                        System.out.println("Taille collection Dans mapfaces : " + simpleFeatures.size());
-                        final ValueExpression ve = comp.getValueExpression("value");
-                        if (ve != null) {
-                            if (ve.getValue(context.getELContext()) instanceof Object) {
+                            final FeatureCollection<SimpleFeatureType, SimpleFeature> simpleFeatures = FacesMapUtils.getSimpleFeaturesFromFeatures(features);
+                            final List<SimpleFeature> sfList = new ArrayList<SimpleFeature>();
+                            final FeatureIterator<SimpleFeature> featIt = simpleFeatures.features();
+                            while(featIt.hasNext()) {
+                                sfList.add(featIt.next());
+                            }
+                            final ValueExpression ve = comp.getValueExpression("value");
+                            if (ve != null) {
                                 ve.setValue(context.getELContext(), sfList);
                             }
+                            comp.setValue(sfList);
                         }
-                        comp.setValue(sfList);
                     }
                 }
             }
-            System.out.println("FIN DECODE");
         } catch (ParseException ex) {
             Logger.getLogger(SvgLayerRenderer.class.getName()).log(Level.SEVERE, null, ex);
         } catch (FactoryException ex) {
