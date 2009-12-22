66d81a5fd8d92d4ac1f14bd700332e8e358acc93
mediii <mediii@435c697f-6949-0410-9f2e-81b55f2b53f5>
2009-12-22 15:48:15 +0000

    Examples : added a demo page for base treetable component.

--- web/examples/src/main/java/org/mapfaces/demo/bean/TreetableBean.java
+++ web/examples/src/main/java/org/mapfaces/demo/bean/TreetableBean.java
@@ -13,6 +13,7 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
+
 package org.mapfaces.demo.bean;
 
 import java.io.FileNotFoundException;
@@ -20,33 +21,46 @@ import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.tree.DefaultMutableTreeNode;
 import javax.xml.bind.JAXBException;
 import org.mapfaces.adapter.owc.Adapter;
 import javax.swing.tree.DefaultTreeModel;
+import org.mapfaces.models.tree.TreeItem;
 
 /**
  *
- * @author Kevin Delfour (IRD)
+ * @author Kevin Delfour
  */
 public class TreetableBean {
 
     private static final Logger LOGGER = Logger.getLogger(TreetableBean.class.getName());
     private DefaultTreeModel tree;
+    private DefaultTreeModel treemodel;
 
     public TreetableBean() {
-        String fileUrl = "data/context/owc030Cut.xml";
-        try {
-            tree = Adapter.OWC2Tree(fileUrl);
+//        String fileUrl = "data/context/owc030Cut.xml";
+//        try {
+//            tree = Adapter.OWC2Tree(fileUrl);
+//
+//        } catch (JAXBException ex) {
+//            LOGGER.log(Level.SEVERE, null, ex);
+//        } catch (UnsupportedEncodingException ex) {
+//            LOGGER.log(Level.SEVERE, null, ex);
+//        } catch (FileNotFoundException ex) {
+//            LOGGER.log(Level.SEVERE, null, ex);
+//        } catch (MalformedURLException ex) {
+//            LOGGER.log(Level.SEVERE, null, ex);
+//        }
 
-        } catch (JAXBException ex) {
-            LOGGER.log(Level.SEVERE, null, ex);
-        } catch (UnsupportedEncodingException ex) {
-            LOGGER.log(Level.SEVERE, null, ex);
-        } catch (FileNotFoundException ex) {
-            LOGGER.log(Level.SEVERE, null, ex);
-        } catch (MalformedURLException ex) {
-            LOGGER.log(Level.SEVERE, null, ex);
-        }
+        TreeItem treeItem = new TreeItem("root", "metadata root");
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode(treeItem);
+        root.add(new DefaultMutableTreeNode(new TreeItem("title1","Citation")));
+        root.add(new DefaultMutableTreeNode(new TreeItem("title2","Responsible party")));
+        root.add(new DefaultMutableTreeNode(new TreeItem("title3","Geographic Extent")));
+        root.add(new DefaultMutableTreeNode(new TreeItem("title4","Contacts")));
+        root.add(new DefaultMutableTreeNode(new TreeItem("title5","Tempoaral Extent")));
+        treemodel = new DefaultTreeModel(root);
+
     }
 
     public DefaultTreeModel getTree() {
@@ -55,5 +69,19 @@ public class TreetableBean {
 
     public void setTree(DefaultTreeModel newvalue) {
         this.tree = newvalue;
+    }
+
+    /**
+     * @return the treemodel
+     */
+    public DefaultTreeModel getTreemodel() {
+        return treemodel;
+    }
+
+    /**
+     * @param treemodel the treemodel to set
+     */
+    public void setTreemodel(DefaultTreeModel treemodel) {
+        this.treemodel = treemodel;
     }
 }
