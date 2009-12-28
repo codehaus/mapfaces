bafa29370ac1b3ec0fd07a04e9e35565e014cedd
lpratlong <lpratlong@435c697f-6949-0410-9f2e-81b55f2b53f5>
2009-12-28 11:08:41 +0000

    Delete of Test class used to track a bug.

--- trunk/web/examples/src/main/java/org/mapfaces/demo/bean/TreetableBean.java
+++ trunk/web/examples/src/main/java/org/mapfaces/demo/bean/TreetableBean.java
@@ -16,16 +16,9 @@
 
 package org.mapfaces.demo.bean;
 
-import java.io.FileNotFoundException;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.xml.bind.JAXBException;
-import org.mapfaces.adapter.owc.Adapter;
 import javax.swing.tree.DefaultTreeModel;
-import org.mapfaces.demo.model.Test;
 import org.mapfaces.models.tree.TreeItem;
 
 /**
@@ -41,7 +34,6 @@ public class TreetableBean {
     private static final Logger LOGGER = Logger.getLogger(TreetableBean.class.getName());
     private DefaultTreeModel tree;
     private DefaultTreeModel treemodel;
-    private Test test = new Test("title1","value1");
     private String type = "text";
 
     public TreetableBean() {
@@ -58,7 +50,6 @@ public class TreetableBean {
 //        } catch (MalformedURLException ex) {
 //            LOGGER.log(Level.SEVERE, null, ex);
 //        }
-        System.out.println("TEST : title => " + test.getTitle() + " - value => " + test.getValue());
         TreeItem treeItem = new TreeItem("root", "metadata root");
         DefaultMutableTreeNode root = new DefaultMutableTreeNode(treeItem);
         root.add(new DefaultMutableTreeNode(new TreeItem("title1","Citation")));
@@ -91,21 +82,7 @@ public class TreetableBean {
     public void setTreemodel(DefaultTreeModel treemodel) {
         this.treemodel = treemodel;
     }
-
-    /**
-     * @return the test
-     */
-    public Test getTest() {
-        return test;
-    }
-
-    /**
-     * @param test the test to set
-     */
-    public void setTest(Test test) {
-        this.test = test;
-    }
-
+    
     /**
      * @return the type
      */
